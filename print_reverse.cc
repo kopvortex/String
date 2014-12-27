d3a8a7a19e71fd04d751c6ae460e1622534d22be
koshy <koshy1989@gmail.com>
2014-12-27 16:43:21 -0700

    C++ program to reverse a string

---
+++ print_reverse.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void reverse_str(char *str) {
+    if(*str) {
+        reverse_str(str+1);
+        cout<<*str;
+    }
+}
+
+int main() {
+    char str[256];
+    cout<<"\n Enter a string to reverse"<<endl;
+    cin.getline(str,256);
+    reverse_str(str);
+    cout<<"\n";
+    return 0;
+}
